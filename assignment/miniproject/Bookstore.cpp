1b594808ad8e4486508cb5796166fd555fb56a79
Stina Karnlind <stinakarnlind@gmail.com>
2022-04-21 18:20:04 +0200

    Commit of Bookstore project

--- assignment/miniproject/Bookstore.cpp
+++ assignment/miniproject/Bookstore.cpp
@@ -1,101 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "Bookstore.hpp"
 using namespace std;
-
-bool user_exit = false;
-
-class Book
-{
-public:
-    string author_name, publisher_name, b_title;
-    int price, copies;
-
-    Book(string a, string t, string p, int pr, int c)
-    {
-        author_name = a;
-        publisher_name = p;
-        b_title = t;
-        price = pr;
-        copies = c;
-    }
-    Book()
-    {
-        author_name = " ";
-        publisher_name = " ";
-        b_title = " ";
-        price = 0;
-        copies = 0;
-    }
-};
-
-void print_menu();
-void add_books(vector<Book> &booklist);
-void menu_selection(vector<Book> &booklist);
-void buy_book(vector<Book> &booklist, int index);
-int search_book(vector<Book> &booklist);
-void edit_book(vector<Book> &booklist, int counter);
-
-void print_menu()
-{
-    cout << " ------MENU------ " << endl;
-    cout << "1. Entry of New Book " << endl;
-    cout << "2. Buy Book " << endl;
-    cout << "3. Search for Book " << endl;
-    cout << "4. Edit Details of Book " << endl;
-    cout << "5. Exit " << endl;
-    cout << endl;
-}
 
 void menu_selection(vector<Book> &booklist)
 {
-    cout << "Enter your Choice: " << endl;
-    int value;
-    cin >> value;
+    int value = 0;
 
-    // do
-    // {
-    switch (value)
+    while (value != 5)
     {
-    case 1:
-        add_books(booklist);
-        break;
-    case 2:
-    {
-        int index = search_book(booklist);
-        buy_book(booklist, index);
-        break;
-    }
-    case 3:
-    {
-        search_book(booklist);
-        break;
-    }
-    case 4:
-    {
-        int index=search_book(booklist);
-        edit_book(booklist,index);
-        break;
-    }
-    case 5:
-        cout << "Exits Menu" << endl;
-        // bool user_exit = true;
-        break;
-    default:
-        cout << "Please select an option number from the menu: " << endl;
-    }
-    cout << "\nPress enter to continue..." << endl;
-    cin.ignore(10, '\n');
-    cin.get();
-    // }while(value!=5);
 
-    /*
-        cout << "\n (menu_selection)Available books: " << endl;
-        for (unsigned counter=0; counter<booklist.size(); ++counter)
+        cout << "\n ------MENU------ " << endl;
+        cout << "1. Entry of New Book " << endl;
+        cout << "2. Buy Book " << endl;
+        cout << "3. Search for Book " << endl;
+        cout << "4. Edit Details of Book " << endl;
+        cout << "5. Exit " << endl;
+        cout << endl;
+
+        cout << "Enter your Choice: " << endl;
+
+        cin >> value;
+        switch (value)
         {
-            cout << ' ' << counter << "-";
-            cout << ' ' << booklist[counter].b_title << endl;
-        }*/
+        case 1:
+            add_books(booklist);
+            break;
+        case 2:
+        {
+            int index = search_book(booklist);
+            buy_book(booklist, index);
+            break;
+        }
+        case 3:
+        {
+            search_book(booklist);
+            break;
+        }
+        case 4:
+        {
+            int index = search_book(booklist);
+            edit_book(booklist, index);
+            break;
+        }
+        case 5:
+        {
+            cout<<"Exits program"<<endl;
+            return;
+        }
+        default:
+            cout << "Please select an option number from the menu: " << endl;
+        }
+      
+        cout << "\nPress enter to continue..." << endl;
+        cin.ignore(5,'\n');
+        
+    }
 }
 
 void add_books(vector<Book> &booklist)
@@ -116,31 +77,37 @@ void add_books(vector<Book> &booklist)
 
     Book book(a_name, t_name, p_name, price, copies);
     booklist.push_back(book);
+
+    cin.ignore();
 }
 void buy_book(vector<Book> &booklist, int index)
 {
     int cop;
+    if (booklist.size() > index)
+    {
+        cout << "How many copies would you like?: " << endl;
+        cin >> cop;
 
-    cout << "How many copies would you like?: " << endl;
-    cin >> cop;
-    if (cop <= booklist[index].copies)
-    {
-        cout << "Total cost: " << booklist[index].price * cop << endl;
+        if (cop <= booklist[index].copies)
+        {
+            cout << "Total cost: " << booklist[index].price * cop << endl;
+        }
+        else
+        {
+            cout << "Number of Copies not in Stock";
+        }
     }
-    else
-    {
-        cout << "Number of Copies not in Stock";
-    }
-    // Access the book to decrease the number of available copies
+    cin.ignore();
 }
 int search_book(vector<Book> &booklist)
 {
     string a_name, t_name;
-
-    cout << "\n Enter Author Name: " << endl;
-    cin >> a_name;
-    cout << "\n Enter Title Name: " << endl;
-    cin >> t_name;
+    cout << "\n Enter Author Name: ";
+    cin.ignore();
+    getline(cin, a_name);
+    cout << "\n Enter Title Name: ";
+    cin.ignore();
+    getline(cin, t_name);
 
     bool book_found = false;
     int index;
@@ -165,6 +132,7 @@ int search_book(vector<Book> &booklist)
     {
         cout << "Book NOT found" << endl;
     }
+
     return index;
 }
 
@@ -173,38 +141,18 @@ void edit_book(vector<Book> &booklist, int counter)
     string a_name, p_name, t_name;
     int price, copies;
 
-            cout << "\n Enter Author Name: " << endl;
-            cin >> booklist[counter].author_name;
-            cout << "\n Enter Title Name: " << endl;
-            cin >> booklist[counter].b_title;
-            cout << "\n Enter Publisher Name: " << endl;
-            cin >> booklist[counter].publisher_name;
-            cout << "\n Enter Price: " << endl;
-            cin >> booklist[counter].price;
-            cout << "\n Enter Number of Copies: " << endl;
-            cin >> booklist[counter].copies; 
-}
-
-int main()
-{
-    vector<Book> booklist;
-    // books to start with
-    Book book1("Stina", "Kalle Anka", "Frank", 200, 2);
-    Book book2("Astrid Lindgren", "Bullerbyn", "Ilon Wiklund", 55, 76);
-    Book book3("Gunilla Bergström", "Alfons Åberg", "Bonnier", 150, 53);
-    Book book4("Astrid L", "Madicken", "Frank", 45, 300);
-
-    booklist.push_back(book1);
-    booklist.push_back(book2);
-    booklist.push_back(book3);
-    booklist.push_back(book4);
-
-    bool user_exit = false;
-    do
+    if (counter < booklist.size())
     {
-        print_menu();
-        menu_selection(booklist);
-    } while (true);
-
-    return 0;
+        cout << "\n Enter Author Name: " << endl;
+        cin >> booklist[counter].author_name;
+        cout << "\n Enter Title Name: " << endl;
+        cin >> booklist[counter].b_title;
+        cout << "\n Enter Publisher Name: " << endl;
+        cin >> booklist[counter].publisher_name;
+        cout << "\n Enter Price: " << endl;
+        cin >> booklist[counter].price;
+        cout << "\n Enter Number of Copies: " << endl;
+        cin >> booklist[counter].copies;
+    }
+    cin.ignore();
 }
